398685d66bab20f9c28937c35bc682f2ea8cb867
HaMy1122 <jjune1415@gmail.com>
2022-11-04 20:47:16 +0700

    Chuyen doi USD ra VND

---
+++ chuyen_doi_tien_te.py
@@ -0,0 +1,12 @@
+import turtle
+import math
+
+
+USD = float(input("Nhap so tien USD: "))
+ti_gia = float(input("Nhap ti gia USD/VND: "))
+
+VND = USD * ti_gia
+print("So tien VND = ",VND)
+
+turtle.done()
+
